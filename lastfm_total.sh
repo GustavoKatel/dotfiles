163ae22a2425fa8464aa1d4e855bd7cd6d1e4384
Gustavo Brito <gbritosampaio@gmail.com>
2014-05-15 03:21:12 -0300

    Link to real file

--- lastfm_total.sh
+++ lastfm_total.sh
@@ -1,1 +1,3 @@
-/media/Arquivos/g5/bash/playcount.sh
+#!/bin/bash
+
+curl http://ws.audioscrobbler.com/2.0/\?method\=user.getinfo\&user\=GustavoKatel\&api_key\=8a40f5950e301d95268f45c640cdd654\&format\=json 2> /dev/null | grep -o "playcount\":\"[0-9]\+\"" | grep -o "[0-9]\+"
--- lastfm_total_persong.sh
+++ lastfm_total_persong.sh
@@ -1,1 +1,3 @@
-/media/Arquivos/g5/bash/playcount_persong.sh
+#!/bin/bash
+
+a=`clementine-info %a | sed "s/ /+/g"`; t=`clementine-info %t | sed "s/ /+/g"`; curl http://ws.audioscrobbler.com/2.0/\?method\=track.getInfo\&username\=GustavoKatel\&api_key\=8a40f5950e301d95268f45c640cdd654\&format\=json\&track\=$t\&artist\=$a\&autocorrect\=1 2> /dev/null | grep -o "userplaycount\":\"[0-9]\+\"" | grep -o "[0-9]\+"
