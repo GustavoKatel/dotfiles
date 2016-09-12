75d84775cfa7ba3c9040222d881145bf378d3bfc
Gustavo Sampaio <gbritosampaio@gmail.com>
2016-09-12 20:02:29 -0300

    Aliases and functions

---
+++ .config/fish/config.fish
@@ -0,0 +1,16 @@
+alias myip="curl http://myip.dnsomatic.com ; and echo ''"
+
+alias clipi="xsel -b -i"
+alias clipo="xsel -b -o"
+
+alias g5="cd /media/Arquivos/g5"
+
+alias q="exit"
+
+alias upgrade="sudo apt update ; and sudo apt upgrade"
+
+function pretty-json
+    python -mjson.tool
+end
+
+set -x EDITOR 'vim'
