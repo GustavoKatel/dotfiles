dfd971c7bde5a123b90841d88ae79ebd0d8707eb
Gustavo Sampaio <gbritosampaio@gmail.com>
2016-09-18 18:43:27 -0300

    Bugfix: test if $CMD_DURATION is set in fish_right_prompt.fish

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -7,10 +7,16 @@ function fish_right_prompt
   set -l normal (set_color normal)
 
   set -l duration ""
-  if test $CMD_DURATION -gt 1000
-    set duration " ~"(printf "%.1fs " (math "$CMD_DURATION / 1000"))" | "
+  if test -n "$CMD_DURATION"
+
+    if test $CMD_DURATION -gt 60000
+      set duration " ~"(printf "%.1fm " (math "$CMD_DURATION / 60000"))
+    else if test $CMD_DURATION -gt 1000
+      set duration " ~"(printf "%.1fs " (math "$CMD_DURATION / 1000"))
+    end
+
   end
 
-  echo -n $yellow$duration $red"δ » Δ | λ » Λ" $normal(date +%H:%M:%S)
+  echo -n $yellow$duration$red"δ » Δ | λ » Λ" $cyan(date +%H:%M:%S)
 
 end
