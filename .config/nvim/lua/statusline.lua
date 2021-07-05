9350cba6cd05f63831bc1572cc26a400a5ae7d29
Gustavo Sampaio <gbritosampaio@gmail.com>
2021-07-05 14:04:42 -0300

    Mon Jul  5 10:04:42 PDT 2021

--- .config/nvim/lua/statusline.lua
+++ .config/nvim/lua/statusline.lua
@@ -1,12 +1,25 @@
 local v = require("utils")
-
 local lualine = require('lualine')
+
+local function lualine_custom_winnr()
+    return " "..v.fn.winnr()
+end
 
 lualine.setup({
     options = {
         theme = "codedark",
     },
     sections = {
-        lualine_x = {{'diagnostics', {sources = {"coc"}}}, 'encoding', 'fileformat', 'filetype'}
+        lualine_c = {
+            {
+              'filename',
+              file_status = false, -- displays file status (readonly status, modified status)
+              path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
+            }
+        },
+        lualine_x = {{lualine_custom_winnr}, 'diff', 'fileformat', 'filetype'}
+    },
+    inactive_sections = {
+        lualine_x = {{lualine_custom_winnr}, 'location'}
     }
 })
