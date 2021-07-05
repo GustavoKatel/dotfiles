fd7f09176d05bc86aba5e57761f6d38ee402802a
Gustavo Sampaio <gbritosampaio@gmail.com>
2021-07-04 23:33:22 -0300

    Sun Jul  4 19:33:22 PDT 2021

--- .config/nvim/lua/statusline.lua
+++ .config/nvim/lua/statusline.lua
@@ -3,6 +3,9 @@ local v = require("utils")
 local lualine = require('lualine')
 
 lualine.setup({
+    options = {
+        theme = "codedark",
+    },
     sections = {
         lualine_x = {{'diagnostics', {sources = {"coc"}}}, 'encoding', 'fileformat', 'filetype'}
     }
