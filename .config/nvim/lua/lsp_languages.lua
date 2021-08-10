b76823171e8bc54dc6371e726f702722c0d5baf0
Gustavo Sampaio <gbritosampaio@gmail.com>
2021-08-10 19:06:51 -0300

    Tue Aug 10 15:06:51 PDT 2021

--- .config/nvim/lua/lsp_languages.lua
+++ .config/nvim/lua/lsp_languages.lua
@@ -1,9 +1,11 @@
 
 local eslint = {
-    lintCommand = './node_modules/.bin/eslint -f visualstudio --stdin --stdin-filename ${INPUT}',
-    lintIgnoreExitCode = true,
-    lintStdin = true,
-    lintFormats = {"%f(%l,%c): %tarning %m", "%f(%l,%c): %rror %m"}
+  lintCommand = "eslint_d -f unix --stdin --stdin-filename ${INPUT}",
+  lintStdin = true,
+  lintFormats = {"%f:%l:%c: %m"},
+  lintIgnoreExitCode = true,
+  formatCommand = "eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}",
+  formatStdin = true
 }
 
 local prettier = {
