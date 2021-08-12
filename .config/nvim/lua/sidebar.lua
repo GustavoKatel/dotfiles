fa6626c9c3dcd68218c5d9eec31ad99b2e49dd75
Gustavo Sampaio <gbritosampaio@gmail.com>
2021-08-12 10:35:53 -0300

    Thu Aug 12 06:35:53 PDT 2021

--- .config/nvim/lua/sidebar.lua
+++ .config/nvim/lua/sidebar.lua
@@ -1,3 +1,7 @@
+local v = require("utils")
+
 local sidebar = require("sidebar-nvim")
-sidebar.setup({})
+sidebar.setup()
 sidebar.open()
+
+v.cmd.highlight("SidebarNvimLspDiagnosticsTotalNumber guifg=Black")
