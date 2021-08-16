5bc907246fd69c86a2fd8e0e64fcbf59af1f776e
Gustavo Sampaio <gbritosampaio@gmail.com>
2021-08-16 18:13:17 -0300

    Mon Aug 16 14:13:17 PDT 2021

--- .config/nvim/lua/asynctasks.lua
+++ .config/nvim/lua/asynctasks.lua
@@ -8,3 +8,4 @@ vim.cmd("let g:asyncrun_runner = get(g:, 'asyncrun_runner', {})")
 vim.cmd("let g:asyncrun_runner.floaterm = function('AsyncTasksRunnerFloaterm')")
 v.v.g.asynctasks_term_pos = "floaterm"
 
+v.v.g.asynctasks_config_name = ".nvim/tasks.ini"
