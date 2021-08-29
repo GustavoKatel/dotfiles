da3143c29f2684fc7364c384055728d621b77068
Gustavo Sampaio <gbritosampaio@gmail.com>
2021-08-29 15:25:12 -0300

    Sun Aug 29 11:25:12 PDT 2021

--- .config/nvim/lua/sidebar.lua
+++ .config/nvim/lua/sidebar.lua
@@ -4,8 +4,8 @@ local user_profile = require("user_profile")
 local sidebar = require("sidebar-nvim")
 sidebar.setup({
     sections = user_profile.with_profile_table({
-        default = {"datetime", "git-status", "lsp-diagnostics"},
-        work = {"datetime", "git-status", "lsp-diagnostics"}
+        default = {"datetime", "git-status", "lsp-diagnostics", "todos"},
+        work = {"datetime", "git-status", "lsp-diagnostics", "todos"}
     })
 })
 sidebar.open()
