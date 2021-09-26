2bee0d0ef275b8b1c6651dc02e1ea49461b23c9c
Gustavo Sampaio <gbritosampaio@gmail.com>
2021-09-26 15:32:19 -0300

    Sun Sep 26 11:32:19 PDT 2021

--- .config/nvim/lua/statusline_theme.lua
+++ .config/nvim/lua/statusline_theme.lua
@@ -1,6 +1,6 @@
 local colors = require("material.colors")
-local theme = require("lualine.themes.material-nvim")
---local theme = require('lualine.themes.onedark')
+--local theme = require("lualine.themes.material-nvim")
+local theme = require("lualine.themes.onedark")
 
 local white_dimmed_mapping = { onedark = "#8c8c8c", material = "#ababab" }
 local white_dimmed = white_dimmed_mapping[vim.g.colors_name]
