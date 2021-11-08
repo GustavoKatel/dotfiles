3f5a5973d906eb464394a7108f6dfdf383fb73a7
Gustavo Sampaio <gbritosampaio@gmail.com>
2021-11-08 17:02:05 -0300

    Mon Nov  8 12:02:05 PST 2021

--- dev/shell_recursive_counter.sh
+++ dev/shell_recursive_counter.sh
@@ -11,6 +11,16 @@ function shell_counter() {
       ignore_value=2
   fi
 
+  if [ -n "$KITTY_PID" ]; then
+      # kitty adds one extra session too
+      ignore_value=1
+
+      # nvim terminals when inside kitty
+      if [ -n "$NVIM_LISTEN_ADDRESS" ]; then
+          ignore_value=2
+      fi
+  fi
+
   counter=$(($SHELL_RECURSIVE_COUNTER-$ignore_value))
 
   test $counter -le 1 && exit 1;
