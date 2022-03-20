556a080a8cf7a537a2801febf3cf49fed76bd9cf
Gustavo Sampaio <gbritosampaio@gmail.com>
2022-03-20 18:18:07 -0300

    Sun Mar 20 14:18:07 PDT 2022

--- .config/nvim/lua/asynctasks.lua
+++ .config/nvim/lua/asynctasks.lua
@@ -4,8 +4,15 @@ v.fn["AsyncTasksRunnerFloaterm"] = function(opts)
 	v.cmd.FloatermNew(opts.cmd)
 end
 
+v.fn["AsyncTasksRunnerHarpoon"] = function(opts)
+	require("harpoon.term").sendCommand(1, opts.cmd .. "\n")
+	require("harpoon.term").gotoTerminal(1)
+end
+
 vim.cmd("let g:asyncrun_runner = get(g:, 'asyncrun_runner', {})")
 vim.cmd("let g:asyncrun_runner.floaterm = function('AsyncTasksRunnerFloaterm')")
-v.v.g.asynctasks_term_pos = "floaterm"
+vim.cmd("let g:asyncrun_runner.harpoon = function('AsyncTasksRunnerHarpoon')")
+--v.v.g.asynctasks_term_pos = "floaterm"
+v.v.g.asynctasks_term_pos = "harpoon"
 
 v.v.g.asynctasks_config_name = ".nvim/tasks.ini"
