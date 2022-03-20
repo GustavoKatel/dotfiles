7ad51ffaf9f8891098a3ea36aa9d637801e3a2a4
Gustavo Sampaio <gbritosampaio@gmail.com>
2022-03-20 17:55:51 -0300

    Sun Mar 20 13:55:51 PDT 2022

--- .config/nvim/lua/scratches.lua
+++ .config/nvim/lua/scratches.lua
@@ -1,7 +1,11 @@
 -- some helpers to manage scratches
 local v = require("utils")
 
-local M = {}
+local M = {
+	_state = {
+		last_floating_window = nil,
+	},
+}
 
 function M.get_scratch_filename()
 	return ".scratches/notes.md"
@@ -14,6 +18,11 @@ end
 vim.api.nvim_add_user_command("ScratchOpenSplit", M.open_scratch_file, {})
 
 function M.open_scratch_file_floating(opts)
+	if M._state.last_floating_window ~= nil then
+		vim.api.nvim_win_close(M._state.last_floating_window, false)
+		M._state.last_floating_window = nil
+	end
+
 	opts = vim.tbl_deep_extend("force", { percentWidth = 0.8, percentHeight = 0.8 }, opts or {})
 
 	-- Get the current UI
@@ -23,7 +32,7 @@ function M.open_scratch_file_floating(opts)
 	local height = math.floor(ui.height * opts.percentHeight)
 
 	-- Create the floating window
-	local opts = {
+	local win_opts = {
 		relative = "editor",
 		width = width,
 		height = height,
@@ -33,7 +42,8 @@ function M.open_scratch_file_floating(opts)
 		--style = "minimal",
 		border = "rounded",
 	}
-	local win = vim.api.nvim_open_win(0, true, opts)
+	local winnr = vim.api.nvim_open_win(0, true, win_opts)
+	M._state.last_floating_window = winnr
 
 	vim.api.nvim_command("edit " .. M.get_scratch_filename())
 
@@ -42,7 +52,10 @@ function M.open_scratch_file_floating(opts)
 	local closing_keys = { "q", "<ESC>" }
 
 	for _, key in ipairs(closing_keys) do
-		vim.keymap.set({ "n" }, key, ":close<CR>", { buffer = bufnr })
+		vim.keymap.set({ "n" }, key, function()
+			vim.api.nvim_win_close(0, false)
+			M._state.last_floating_window = nil
+		end, { buffer = bufnr })
 	end
 end
 vim.api.nvim_add_user_command("ScratchOpenFloat", M.open_scratch_file_floating, {})
