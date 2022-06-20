0cd394f047c122297aa73b28fa852dfaf665f1fb
Gustavo Sampaio <gbritosampaio@gmail.com>
2022-06-20 09:25:37 -0300

    Mon Jun 20 05:25:37 PDT 2022

--- .config/nvim/lua/sidebar.lua
+++ .config/nvim/lua/sidebar.lua
@@ -1,13 +1,23 @@
 local v = require("utils")
 local user_profile = require("uprofile")
 
+local git_section = require("sidebar-nvim.builtin.git")
+git_section.bindings["a"] = git_section.bindings["s"]
+
 local sidebar = require("sidebar-nvim")
+
 sidebar.setup({
 	open = true,
 	initial_width = 40,
 	enable_profile = false,
 	hide_statusline = false,
-	--section_separator = { "AAAAA", "BBB" },
+	section_separator = { "-----", "" },
+	--section_title_separator = function(section, index)
+	--return { index .. "" }
+	--end,
+	--section_separator = function(section, index)
+	--return { "-----" }
+	--end,
 	sections = user_profile.with_profile_table({
 		default = { "git", "diagnostics", require("dap-sidebar-nvim.breakpoints"), "containers" },
 		test = {
@@ -23,8 +33,8 @@ sidebar.setup({
 	}),
 	datetime = { clocks = { { name = "Local" }, { tz = "America/Los_Angeles" }, { tz = "Etc/UTC" } } },
 	todos = { initially_closed = true },
-	buffers = { sorting = "name" },
-	files = {},
+	buffers = { sorting = "name", ignore_not_loaded = true },
+	files = { follow = false },
 })
 
 v.cmd.highlight("link SidebarNvimLspDiagnosticsTotalNumber Normal")
