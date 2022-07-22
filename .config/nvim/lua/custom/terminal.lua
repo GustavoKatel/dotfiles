a52bb638293c28301988d7df7c8a5f06e4a5db9c
Gustavo Sampaio <gbritosampaio@gmail.com>
2022-07-22 17:23:43 -0300

    Fri Jul 22 13:23:43 PDT 2022

--- .config/nvim/lua/custom/terminal.lua
+++ .config/nvim/lua/custom/terminal.lua
@@ -2,6 +2,19 @@ local M = {}
 
 M.term_bufnr = nil
 
+local function set_autocommand_to_terminal(bufnr)
+	local group = "custom_terminal_sticky"
+
+	vim.api.nvim_create_augroup(group, { clear = true })
+	vim.api.nvim_create_autocmd({ "TermClose" }, {
+		group = group,
+		buffer = bufnr,
+		callback = function()
+			M.term_bufnr = nil
+		end,
+	})
+end
+
 function M.init_or_attach()
 	if M.term_bufnr == nil then
 		local current_bufnr = vim.api.nvim_get_current_buf()
@@ -9,10 +22,12 @@ function M.init_or_attach()
 
 		if buftype == "terminal" then
 			M.term_bufnr = current_bufnr
+			set_autocommand_to_terminal(current_bufnr)
 			vim.notify("terminal set", vim.log.levels.INFO)
 		else
 			vim.cmd("terminal")
 			M.term_bufnr = vim.api.nvim_get_current_buf()
+			set_autocommand_to_terminal(M.term_bufnr)
 			vim.notify("terminal created", vim.log.levels.INFO)
 		end
 
