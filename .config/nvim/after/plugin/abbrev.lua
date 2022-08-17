db51e19ec1777891c6f22ebf7f707ebfb677e50f
Gustavo Sampaio <gbritosampaio@gmail.com>
2022-08-17 10:27:43 -0300

    Wed Aug 17 06:27:43 PDT 2022

--- .config/nvim/after/plugin/abbrev.lua
+++ .config/nvim/after/plugin/abbrev.lua
@@ -1,3 +1,4 @@
 vim.cmd("Abolish {despa,sepe}rat{e,es,ed,ing,ely,ion,ions,or}  {despe,sepa}rat{}")
 vim.cmd("Abolish fotter  footer")
 vim.cmd("Abolish tempalte{,s}  template{}")
+vim.cmd("Abolish unkown  unknown")
