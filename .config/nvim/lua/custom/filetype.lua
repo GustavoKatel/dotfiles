3af65439ec8741b3f20e5379d8a331d8f6af1005
Gustavo Sampaio <gbritosampaio@gmail.com>
2022-09-30 13:37:44 -0300

    Fri Sep 30 09:37:44 PDT 2022

--- .config/nvim/lua/custom/filetype.lua
+++ .config/nvim/lua/custom/filetype.lua
@@ -1,19 +1,16 @@
 -- https://www.reddit.com/r/neovim/comments/rvwsl3/introducing_filetypelua_and_a_call_for_help/
--- TODO: remove this after 0.8
-vim.g.do_filetype_lua = 1
 
 -- these json files are actually jsonc
--- TODO: not working 😢
--- vim.filetype.add({
--- 	pattern = {
--- 		[".*/.vscode/.*.json"] = "jsonc",
--- 		["tsconfig.json"] = "jsonc",
--- 		[".eslintrc.json"] = "jsonc",
--- 	},
--- })
-
-vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-	group = vim.api.nvim_create_augroup("jsonc_ft_detect", { clear = true }),
-	pattern = { "*/.vscode/*.json", "tsconfig.json", "tsconfig.monorepo.json", ".eslintrc.json" },
-	command = "setlocal filetype=jsonc",
+vim.filetype.add({
+	pattern = {
+		[".*/.vscode/.*.json"] = "jsonc",
+		["tsconfig.json"] = "jsonc",
+		[".eslintrc.json"] = "jsonc",
+	},
 })
+
+-- vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+-- 	group = vim.api.nvim_create_augroup("jsonc_ft_detect", { clear = true }),
+-- 	pattern = { "*/.vscode/*.json", "tsconfig.json", "tsconfig.monorepo.json", ".eslintrc.json" },
+-- 	command = "setlocal filetype=jsonc",
+-- })
