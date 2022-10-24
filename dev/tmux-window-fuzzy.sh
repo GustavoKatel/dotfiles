940a6d7862dd39b26fe76c9e06af173318bbe840
Gustavo Sampaio <gbritosampaio@gmail.com>
2022-10-24 15:05:07 -0300

    Mon Oct 24 11:05:07 PDT 2022

--- dev/tmux-window-fuzzy.sh
+++ dev/tmux-window-fuzzy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tmux select-window -t $(tmux list-windows -F '#I: #W' | fzf | awk '{ print substr($1, 0, length($1)-1) }')
+tmux select-window -t $(tmux list-windows -F '#I: #W (#S)' $* | fzf | awk '{ print substr($1, 0, length($1)-1) }')
