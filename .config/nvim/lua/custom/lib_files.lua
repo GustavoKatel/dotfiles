29cabb2cfe0da9488d5ebe1adb301a83416093dc
Gustavo Sampaio <gbritosampaio@gmail.com>
2022-10-24 18:31:35 -0300

    Mon Oct 24 14:31:35 PDT 2022

--- .config/nvim/lua/custom/lib_files.lua
+++ .config/nvim/lua/custom/lib_files.lua
@@ -4,7 +4,13 @@
 -- and https://neovim.discourse.group/t/is-it-possible-to-disable-lsp-in-node-modules-directory-file/444/4
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 	group = vim.api.nvim_create_augroup("lib_files", { clear = true }),
-	pattern = { "*/node_modules/*", "*/venv/*", "/opt/homebrew/Cellar/go/*", "*/.cargo/registry/src/*" },
+	pattern = {
+		"*/node_modules/*",
+		"*/venv/*",
+		"/opt/homebrew/Cellar/go/*",
+		"*/.cargo/registry/src/*",
+		"*/.rustup/toolchains/*",
+	},
 	desc = "make library files read-only and non-modifiable",
 	callback = function()
 		vim.bo.readonly = true
