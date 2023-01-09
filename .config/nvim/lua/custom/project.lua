16375ad3b3be4b0d75182e857a1d555ef3d2b2d8
Gustavo Sampaio <gbritosampaio@gmail.com>
2023-01-09 10:20:29 -0300

    Mon Jan  9 05:20:29 PST 2023

--- .config/nvim/lua/custom/project.lua
+++ .config/nvim/lua/custom/project.lua
@@ -10,6 +10,14 @@ end
 
 local logger_banner = "[project.nvim]"
 
+local log_level_map = {
+	error = vim.log.levels.ERROR,
+	warn = vim.log.levels.WARN,
+	info = vim.log.levels.INFO,
+	debug = vim.log.levels.DEBUG,
+	trace = vim.log.levels.TRACE,
+}
+
 local logger = {
 	error = wrap_logger_func(logger_banner, vim.log.levels.ERROR),
 	warn = wrap_logger_func(logger_banner, vim.log.levels.WARN),
@@ -30,11 +38,15 @@ M.default_opts = {
 	local_folder_name = ".nvim",
 
 	auto_load = true,
-	auto_create = true,
+	auto_create = false,
+
+	logger_level = vim.log.levels.WARN,
 }
 
 function M.setup(opts)
 	M._opts = vim.tbl_extend("force", {}, M.default_opts, opts or {})
+
+	M.setup_logger()
 
 	-- set initial project metadata, it's overriden later if local file exists
 	M.current = { name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t") }
@@ -48,6 +60,14 @@ function M.setup(opts)
 
 	if M._opts.auto_load then
 		M.setup_auto_load()
+	end
+end
+
+function M.setup_logger()
+	for log_fn_name, level_value in pairs(log_level_map) do
+		if level_value < M._opts.logger_level then
+			logger[log_fn_name] = function() end
+		end
 	end
 end
 
@@ -80,7 +100,7 @@ function M.load()
 	pasync.run(function()
 		local err, fd = pasync.uv.fs_open(M.get_local_filename(), "r", 438)
 		if err then
-			logger.error("could not open file: %s", err)
+			logger.info("could not open file: %s", err)
 			return nil
 		end
 
