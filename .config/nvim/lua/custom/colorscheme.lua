1b2e8d60e423089d5901fca226c1ec23fe1d386d
Gustavo Sampaio <gbritosampaio@gmail.com>
2023-03-06 18:10:36 -0300

    Mon Mar  6 13:10:36 PST 2023

--- .config/nvim/lua/custom/colorscheme.lua
+++ .config/nvim/lua/custom/colorscheme.lua
@@ -1,4 +1,16 @@
 vim.g.material_style = "darker"
 vim.g.material_disable_terminal = true
 
+require("kanagawa").setup({
+	colors = {
+		theme = {
+			all = {
+				ui = {
+					bg_gutter = "none",
+				},
+			},
+		},
+	},
+})
+
 vim.cmd.colorscheme("kanagawa")
