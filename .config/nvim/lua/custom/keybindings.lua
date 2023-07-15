7be22b1d040089491b836ff967be30ec13231e3e
Gustavo Sampaio <gbritosampaio@gmail.com>
2023-07-15 11:19:47 -0300

    Sat Jul 15 07:19:47 PDT 2023

--- .config/nvim/lua/custom/keybindings.lua
+++ .config/nvim/lua/custom/keybindings.lua
@@ -22,7 +22,7 @@ for _, code in ipairs({ "<D-s>", "<C-s>", create_special_keymap("ds") }) do
 end
 
 -- vertical split with ctrl-\ | command-\
-vim.keymap.set({ "n" }, "<D-\\>", ":vsplit<CR>")
+vim.keymap.set({ "n" }, "<D-Bslash>", ":vsplit<CR>")
 vim.keymap.set({ "n" }, "<C-\\>", ":vsplit<CR>")
 vim.keymap.set({ "n" }, create_special_keymap("m\\"), ":vsplit<CR>")
 
@@ -186,7 +186,7 @@ vim.keymap.set({ "n" }, "<leader>f", function()
 end)
 
 -- floaterm keybindings
-for _, code in ipairs({ "<A-F12>", "<M-F12>", create_special_keymap("af12", true, true) }) do
+for _, code in ipairs({ "<A-F12>", "<M-F12>", "<F60>", create_special_keymap("af12", true, true) }) do
 	vim.keymap.set({ "n" }, code, "<cmd>FloatermToggle<cr>")
 	vim.keymap.set({ "i" }, code, "<ESC>:FloatermToggle<CR>")
 	vim.keymap.set({ "t" }, code, "<C-\\><C-N>:FloatermToggle<CR>")
@@ -297,7 +297,7 @@ end
 -- telescope commands
 for _, code in ipairs({
 	"<C-S-P>",
-	"<S-D-P>",
+	"<S-D-p>",
 	"<D-P>",
 	second_leader .. "p",
 	create_special_keymap("csp"),
@@ -322,7 +322,7 @@ vim.keymap.set({ "n" }, "z=", function()
 end)
 
 -- telescope global search
-for _, code in ipairs({ "<C-S-F>", "<C-F>", "<S-D-F>", "<D-F>", create_special_keymap("mf") }) do
+for _, code in ipairs({ "<C-S-F>", "<C-F>", "<S-D-F>", "<D-f>", create_special_keymap("mf") }) do
 	vim.keymap.set({ "n" }, code, function()
 		local rg_arguments = {}
 
