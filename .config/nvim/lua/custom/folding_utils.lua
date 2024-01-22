07293beb41aaddcc0a856f69e87068c6b2ff9bfb
Gustavo Sampaio <gbritosampaio@gmail.com>
2024-01-22 11:09:04 -0300

    Mon Jan 22 06:09:04 PST 2024

--- .config/nvim/lua/custom/folding_utils.lua
+++ .config/nvim/lua/custom/folding_utils.lua
@@ -7,7 +7,7 @@ vim.opt.foldcolumn = "1"
 
 local virtual_text_handler = function(virtText, lnum, endLnum, width, truncate)
 	local newVirtText = {}
-	local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+	local suffix = ("  %d "):format(endLnum - lnum)
 	local sufWidth = vim.fn.strdisplaywidth(suffix)
 	local targetWidth = width - sufWidth
 	local curWidth = 0
