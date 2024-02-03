dd151780173d8ee807c19683f8c08ea5e5117889
Gustavo Sampaio <gbritosampaio@gmail.com>
2024-02-02 22:30:05 -0300

    remove old files

--- dev/task.sh
+++
@@ -1,5 +0,0 @@
-# Quick start with the default Taskfile template
-alias task-init="curl -so Taskfile https://raw.githubusercontent.com/GustavoKatel/Taskfile/master/Taskfile.template && chmod +x Taskfile"
-
-# Run your tasks like: task <task>
-alias task=./Taskfile
