11926ca7c7172ebb5d3529dc09e03d3a8dcc315b
Gustavo Sampaio <gbritosampaio@gmail.com>
2024-02-07 12:48:25 -0300

    Wed Feb  7 07:48:25 PST 2024

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -84,23 +84,26 @@ require("lazy").setup({
 		ft = { "markdown" },
 	},
 
-	{
-		"ray-x/go.nvim",
-		dependencies = { -- optional packages
-			"ray-x/guihua.lua",
-			"neovim/nvim-lspconfig",
-			"nvim-treesitter/nvim-treesitter",
+	user_profile.with_profile_table({
+		work = {},
+		default = {
+			"ray-x/go.nvim",
+			dependencies = { -- optional packages
+				"ray-x/guihua.lua",
+				"neovim/nvim-lspconfig",
+				"nvim-treesitter/nvim-treesitter",
+			},
+			config = function()
+				require("go").setup({
+					dap_debug = true,
+					dap_debug_gui = true,
+				})
+			end,
+			event = { "CmdlineEnter" },
+			ft = { "go", "gomod" },
+			build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
 		},
-		config = function()
-			require("go").setup({
-				dap_debug = true,
-				dap_debug_gui = true,
-			})
-		end,
-		event = { "CmdlineEnter" },
-		ft = { "go", "gomod" },
-		build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-	},
+	}),
 
 	-- {
 	-- 	"nvim-neotest/neotest",
