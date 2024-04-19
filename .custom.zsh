15b28578f3a92d180cbb8ccf4e279e5d9bd2ae87
Gustavo Sampaio <gbritosampaio@gmail.com>
2024-04-19 20:32:27 -0300

    Fri Apr 19 16:32:27 PDT 2024

--- .custom.zsh
+++ .custom.zsh
@@ -89,7 +89,7 @@ bindkey '\e[Z' reverse-menu-complete
 [ -f $HOME/.custom_home.zsh ] && source $HOME/.custom_home.zsh
 [ -f $HOME/.custom_private.zsh ] && source $HOME/.custom_private.zsh
 
-export PATH="$HOME/.rd/bin:$PATH"
+# export PATH="$HOME/.rd/bin:$PATH"
 
 eval "$(goenv init -)"
 
