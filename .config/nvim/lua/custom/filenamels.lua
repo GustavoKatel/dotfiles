50f73ea5c26cc47f2847526a30d0a66a626001d3
Gustavo Sampaio <gbritosampaio@gmail.com>
2024-07-01 21:30:50 -0300

    Mon Jul  1 17:30:50 PDT 2024

--- .config/nvim/lua/custom/filenamels.lua
+++ .config/nvim/lua/custom/filenamels.lua
@@ -1,11 +1,24 @@
+local default_opts = {
+	hint = " ",
+
+	filetypes = {
+		"yaml",
+		"go",
+	},
+}
+
 local M = {
 	closing = false,
 }
 
-function M.setup()
+function M.setup(opts)
+	opts = opts or {}
+
+	M.opts = vim.tbl_deep_extend("force", {}, default_opts, opts)
+
 	vim.api.nvim_create_autocmd("FileType", {
 		group = vim.api.nvim_create_augroup("filenamels_lsp_attach", { clear = true }),
-		pattern = "*",
+		pattern = M.opts.filetypes,
 		callback = function()
 			M.start()
 		end,
@@ -30,12 +43,20 @@ function M.client_init(dispatchers)
 	return M
 end
 
-function M.request(method, params, callback, notify_reply_callback)
-	if M[method] ~= nil then
-		return M[method](method, params, callback, notify_reply_callback), 0
+function M.get_hint(filename, folder)
+	if type(M.opts.hint) == "function" then
+		return M.opts.hint(filename, folder)
 	end
 
-	return true, 0
+	return M.opts.hint
+end
+
+function M.request(method, params, callback, notify_reply_callback)
+	if M[method] ~= nil then
+		return M[method](method, params, callback, notify_reply_callback), nil
+	end
+
+	return false, nil
 end
 
 function M.notify(method, params)
@@ -74,6 +95,11 @@ M["textDocument/codeLens"] = function(method, params, callback)
 
 	local ft = vim.api.nvim_buf_get_option(bufnr, "filetype")
 
+	if not ft then
+		callback(nil, nil)
+		return true
+	end
+
 	local codelenses = {}
 
 	local query = vim.treesitter.query.get(ft, "filenames")
@@ -105,7 +131,7 @@ M["textDocument/codeLens"] = function(method, params, callback)
 					["end"] = { line = row2, character = col2 },
 				},
 				command = {
-					title = " ",
+					title = M.get_hint(filename, folder),
 					command = "filenamels.openFile",
 					arguments = { vim.fs.joinpath(folder, filename) },
 				},
